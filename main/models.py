6923c20943057da63c9cfc6239e7838f60245c8b
pixelhubfl <joseluisgirald@gmail.com>
2026-04-04 19:39:31 -0400

    change image field to char

--- main/models.py
+++ main/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True)
-    image = models.ImageField(upload_to='products/', null=True, blank=True)
+    image = models.CharField(max_length=100, null=True, blank=True)
 
     base_price = models.FloatField()
     is_custom_size = models.BooleanField(default=True)
