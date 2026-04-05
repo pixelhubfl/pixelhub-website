ba9f5348561d048ea092cd466dd0816323feabe5
pixelhubfl <joseluisgirald@gmail.com>
2026-04-04 21:56:04 -0400

    add services route

--- main/urls.py
+++ main/urls.py
@@ -4,13 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('shop/', views.shop, name='shop'),
+    path('services/', views.services, name='services'),
 
     path('product/<int:id>/', views.product_detail, name='product_detail'),
 
     path('add-to-cart/<int:id>/', views.add_to_cart, name='add_to_cart'),
     path('cart/', views.cart, name='cart'),
-    path('remove/<str:key>/', views.remove_from_cart, name='remove_from_cart'),
-
-    path('checkout/', views.create_checkout_session, name='checkout'),
-    path('success/', views.success, name='success'),
 ]
