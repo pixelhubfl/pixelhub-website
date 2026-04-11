96a06fbd6136e1a6b71734a828af1b8dea3d8400
pixelhubfl <joseluisgirald@gmail.com>
2026-04-10 22:49:43 -0400

    fix media images permanently

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+# 🔥 IMPORTANTE PARA MEDIA
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('shop/', views.shop, name='shop'),
@@ -14,3 +18,6 @@ urlpatterns = [
     path('success/', views.success, name='success'),
     path('remove/<str:key>/', views.remove_from_cart, name='remove_from_cart'),
 ]
+
+# 🔥 ESTO HACE QUE LAS IMÁGENES DEL ADMIN FUNCIONEN
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
